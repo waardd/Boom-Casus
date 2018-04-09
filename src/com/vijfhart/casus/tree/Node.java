fab7c27c9ce8339fd2525943e3c3a5ca1ca8789b
waardd <waardd@gmail.com>
2018-04-09 20:48:02 +0200

    interface compareLevelTo() toegevoegd

--- src/com/vijfhart/casus/tree/Node.java
+++ src/com/vijfhart/casus/tree/Node.java
@@ -1,7 +1,8 @@
 package com.vijfhart.casus.tree;
 
-public interface Node<E extends Node<E>> {
+public interface Node<E extends Node<E>> extends Comparable {
 
     int getParent();
     int setParent();
+    boolean isLeaf();
 }
