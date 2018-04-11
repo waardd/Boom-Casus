0134db9adbbdd98d65490f054bcb6155db28b400
waardd <waardd@gmail.com>
2018-04-11 11:35:26 +0200

    aanpassing NameNode en TreeApp

--- src/com/vijfhart/casus/tree/NameNode.java
+++ src/com/vijfhart/casus/tree/NameNode.java
@@ -1,8 +1,14 @@
 package com.vijfhart.casus.tree;
 
-public class NameNode implements Node{
+public class NameNode implements Node {
 
     private String naam;
+    private String persnr;
+    private String functie;
+    private String mgr;
+    private String sal;
+    private String toeslag;
+    private String kantnr;
 
     @Override
     public Node getParent() {
@@ -11,7 +17,6 @@ public class NameNode implements Node{
 
     @Override
     public void setParent(Node parent) {
-
     }
 
     @Override
@@ -29,12 +34,68 @@ public class NameNode implements Node{
         return 0;
     }
 
+    public String toString(Object node){
+        return naam+" met functie"+functie;
+    }
+
+    // voor aanmaak nieuwe medewerker
+    public void medewerker(String persnr,String naam,String functie,String mgr,String sal,String toeslag,String kantnr){
+        this.persnr=persnr;
+        this.naam=naam;
+        this.functie=functie;
+        this.mgr=mgr;
+        this.sal=sal;
+        this.toeslag=toeslag;
+        this.kantnr=kantnr;
+    }
+
+    // overload
+    public void setMedewerkerSalaris(String sal, String toeslag) {
+        this.sal = sal;
+        this.toeslag=toeslag;
+    }
+
+    public void setMedewerkerSalaris(String sal) {
+        this.sal = sal;
+    }
+
+    public void setFunctie(String functie) {
+        this.naam = functie;
+    }
+
+    public void setManager(String mgr) {
+        this.mgr = mgr;
+    }
+
+    public boolean equals(){
+        return false;
+    }
+
     public String getNaam() {
         return naam;
     }
 
-    public void setNaam(String naam) {
-        this.naam = naam;
+    public String getPersnr() {
+        return persnr;
     }
 
+    public String getFunctie() {
+        return functie;
+    }
+
+    public String getMgr() {
+        return mgr;
+    }
+
+    public String getSal() {
+        return sal;
+    }
+
+    public String getToeslag() {
+        return toeslag;
+    }
+
+    public String getKantnr() {
+        return kantnr;
+    }
 }
